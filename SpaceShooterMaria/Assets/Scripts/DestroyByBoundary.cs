81d79ec5084334e71b55e4ac20fd526c00b0200c
Maria T <36854099+MariaT7@users.noreply.github.com>
2018-11-27 16:42:13 -0500

    Tutotrial 10 and 11 (1:50)

---
+++ SpaceShooterMaria/Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
