902d18962b4b7dc4a51e007c07e2868841329ef7
Maria T <36854099+MariaT7@users.noreply.github.com>
2018-11-28 17:42:31 -0500

    unity

--- SpaceShooterMaria/Assets/Scripts/DestroyByBoundary.cs
+++ SpaceShooterMaria/Assets/Scripts/DestroyByBoundary.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class DestroyByBoundary : MonoBehaviour
 {
-    public GameObject explosion;
-    public GameObject playerExplosion;
-
     void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Boundary")
-        {
-            return;
-        }
-        Instantiate(explosion, transform.position, transform.rotation);
         Destroy(other.gameObject);
-        Destroy(gameObject);
     }
 }
+
+
+/*public class DestroyByBoundary : MonoBehaviour
+{
+    public GameObject explosion;
+public GameObject playerExplosion;
+
+void OnTriggerExit(Collider other)
+{
+    if (other.tag == "Boundary")
+    {
+        return;
+    }
+    Instantiate(explosion, transform.position, transform.rotation);
+    Destroy(other.gameObject);
+    Destroy(gameObject);
+}
+}*/
--- SpaceShooterMaria/Assets/Scripts/DestroyByContact.cs
+++ SpaceShooterMaria/Assets/Scripts/DestroyByContact.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary")
         {
             return;
         }
-
+        Instantiate(explosion, transform.position, transform.rotation); //as GameObject
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, transform.position, transform.rotation);
+        }
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
