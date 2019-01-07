06bf3c0776f7406c0edf43d824b8b3afe8f2f70f
Maria <36854099+MariaT7@users.noreply.github.com>
2019-01-07 00:56:20 -0500

    update

---
+++ CPTMaria/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+
+
+
+    private int level = 3;
+
+    //Awake is always called before Start functions
+    void Awake()
+    {
+        if (instance == null)
+            //If it is not, set instance to this
+            instance = this;
+        else if (!instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
+    void Update()
+    {
+         
+    }
+
+}
